59f769a56435cc494c69af5c466b1df3471e512c
Emmy <ewong96@live.ca>
2020-11-23 13:38:34 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const port = 4000;
+
+let app = express();
+
+app.use(express.static('public'))
+
+app.get('/',(req, res)=>{
+    res.sendFile(__dirname + '/views/index.html');
+})
+
+app.get('/about',(req, res)=>{
+    res.sendFile(__dirname + '/views/about.html');
+})
+
+app.get('/projects', (req,res)=>{
+    res.sendFile(__dirname + '/views/projects.html')
+})
+
+app.get('/contact', (req,res)=>{
+    res.sendFile(__dirname + '/views/contact.html')
+})
+
+app.get('/project/:id',(req,res)=>{
+    res.sendFile(__dirname + '/views/singleproject.html')
+})
+
+app.listen(port,()=>{
+    console.log('started at port 4000')
+})
